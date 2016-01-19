4e0967c60b2fe0d3c14ee9dd0c0773d1ad760de2
Max Soe <maxsoe@gmail.com>
2016-01-19 06:47:51 +0000

    Two cats

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-var counter = 0;
+$(document).ready(function() {
+  console.log("document loaded");
 
-$('#catphoto').click(function(e) {
-  counter++;
-  $( ".clicks" ).text( counter );
+  // Cats
+  var cat1name = "Sebastian";
+  var cat1counter = 0;
+  var cat2name = "Tom";
+  var cat2counter = 0;
+
+  // Rename cats
+  $(".cat1name").text(cat1name);
+  $(".cat2name").text(cat2name);
+
+  // Increase counters
+  $('.cat1photo').click(function(e) {
+    cat1counter++;
+    $(".cat1clicks").text(cat1counter);
+  });
+
+  $('.cat2photo').click(function(e) {
+    cat2counter++;
+    $(".cat2clicks").text(cat2counter);
+  });
+
 });
